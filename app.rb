ad229de8fb04e2a5fc82653d19ac1260ebdf75e8
brodiethiele <brodie.thiele@gmail.com>
2018-08-02 17:29:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,10 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/food_form' do
+  get '/song_form' do
     erb :food_form
   end
 
   # Add your post route and action below
-
+post '/song' do
+  @song1=params[:song1]
+  @song2=params[:song2]
+  erb :food
+  end
 end
